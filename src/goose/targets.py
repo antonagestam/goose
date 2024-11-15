a2e6e2cabfde56999cd27c4dff391fde91e08e12
Anton Agestam <git@antonagestam.se>
2024-11-15 18:40:32 +0100

    chore: Test target helper functions

--- src/goose/targets.py
+++ src/goose/targets.py
@@ -4,9 +4,11 @@ import enum
 import re
 from collections.abc import AsyncGenerator
 from collections.abc import AsyncIterable
+from collections.abc import Iterable
 from collections.abc import Sequence
 from dataclasses import dataclass
 from pathlib import Path
+from re import Pattern
 from typing import Final
 from typing import assert_never
 
@@ -17,7 +19,6 @@ from goose.process import stream_out
 
 from .config import Config
 from .config import HookConfig
-from .filter import path_matches_patterns
 
 
 @dataclass(frozen=True, slots=True, kw_only=True)
@@ -61,6 +62,8 @@ async def _git_file_list(selector: Selector) -> AsyncGenerator[Path]:
         command = (
             "git",
             "diff",
+            # Note: D is not included here, such that we do not pass deleted files.
+            "--diff-filter=ACMR",
             "--name-only",
             "-z",
             "HEAD",
@@ -77,9 +80,18 @@ async def _git_file_list(selector: Selector) -> AsyncGenerator[Path]:
     assert process.stderr is not None
     stream_stderr = asyncio.create_task(stream_out("[stderr]", process.stderr))
     async for path in _stream_paths(_nil_split_stream(process.stdout)):
+        if not path.exists():
+            continue
         yield path
     await stream_stderr
     await process.wait()
+
+
+def _path_matches_patterns(
+    path: Path,
+    patterns: Iterable[Pattern],
+) -> bool:
+    return any(pattern.search(str(path)) is not None for pattern in patterns)
 
 
 _builtin_excludes: Final = (re.compile(r"^\.goose/.*"),)
@@ -88,7 +100,7 @@ _builtin_excludes: Final = (re.compile(r"^\.goose/.*"),)
 async def get_targets(config: Config, selector: Selector) -> tuple[Target, ...]:
     targets = []
     async for path in _git_file_list(selector):
-        if path_matches_patterns(path, (*config.exclude, *_builtin_excludes)):
+        if _path_matches_patterns(path, (*config.exclude, *_builtin_excludes)):
             continue
         targets.append(
             Target(
@@ -111,5 +123,5 @@ def filter_hook_targets(
         target.path
         for target in targets
         if (not hook.types or target.tags & hook.types)
-        if not path_matches_patterns(target.path, hook.exclude)
+        if not _path_matches_patterns(target.path, hook.exclude)
     )
---
+++ tests/unit/test_targets.py
@@ -0,0 +1,109 @@
+import asyncio
+from asyncio import StreamReader
+from collections.abc import AsyncGenerator
+from contextlib import chdir
+from pathlib import Path
+
+import pytest
+
+from goose.targets import Selector
+from goose.targets import _git_file_list
+from goose.targets import _nil_split_stream
+from goose.targets import _stream_paths
+
+
+async def as_tuple[T](source: AsyncGenerator[T]) -> tuple[T, ...]:
+    parts = []
+    async for part in source:
+        parts.append(part)
+    return tuple(parts)
+
+
+@pytest.mark.parametrize(
+    ("input_bytes", "expected_chunks"),
+    (
+        (b"", ()),
+        (b"foo", ()),
+        (b"hello\n", (b"hello",)),
+        (b"foo\00bar\n", (b"foo", b"bar")),
+        (b"foo\00 bar\00baz \n", (b"foo", b"bar", b"baz")),
+        (b"foo\nbar\n", (b"foo", b"bar")),
+    ),
+)
+async def test_nil_split_stream(
+    input_bytes: bytes,
+    expected_chunks: tuple[bytes, ...],
+) -> None:
+    stream_reader = StreamReader()
+    stream_reader.feed_data(input_bytes)
+    stream_reader.feed_eof()
+    chunks = await as_tuple(_nil_split_stream(stream_reader))
+    assert chunks == expected_chunks
+
+
+async def test_stream_paths() -> None:
+    async def gen() -> AsyncGenerator[bytes]:
+        yield b"a"
+        yield b"b/c"
+        yield b"/d"
+
+    paths = await as_tuple(_stream_paths(gen()))
+    assert paths == (
+        Path("a"),
+        Path("b/c"),
+        Path("/d"),
+    )
+
+
+@pytest.fixture
+async def git_repository(tmp_path: Path) -> AsyncGenerator[Path]:
+    with chdir(tmp_path):
+        create_repository = await asyncio.create_subprocess_exec("git", "init")
+        await create_repository.wait()
+
+        (checked_in := tmp_path / "checked-in.txt").write_text("foo")
+        (staged := tmp_path / "staged.txt").write_text("bar")
+        (tmp_path / "untracked.txt").write_text("baz")
+        (deleted_staged := tmp_path / "deleted-staged.txt").write_text("lorem")
+        (deleted_worktree := tmp_path / "deleted-worktree.txt").write_text("ipsum")
+
+        add_file = await asyncio.create_subprocess_exec(
+            "git",
+            "add",
+            str(checked_in),
+            str(deleted_staged),
+            str(deleted_worktree),
+        )
+        await add_file.wait()
+
+        commit = await asyncio.create_subprocess_exec("git", "commit", "-minitial")
+        await commit.wait()
+
+        deleted_staged.unlink()
+        deleted_worktree.unlink()
+        rm_file = await asyncio.create_subprocess_exec("git", "rm", str(deleted_staged))
+        await rm_file.wait()
+
+        add_file = await asyncio.create_subprocess_exec("git", "add", str(staged))
+        await add_file.wait()
+
+        yield tmp_path
+
+
+class TestGitFileList:
+    async def test_listing_all_includes_commited_and_staged(
+        self,
+        git_repository: Path,
+    ) -> None:
+        result = await as_tuple(_git_file_list(Selector.all))
+        assert result == (
+            Path("checked-in.txt"),
+            Path("staged.txt"),
+        )
+
+    async def test_listing_diff_includes_only_staged(
+        self,
+        git_repository: Path,
+    ) -> None:
+        result = await as_tuple(_git_file_list(Selector.diff))
+        assert result == (Path("staged.txt"),)
