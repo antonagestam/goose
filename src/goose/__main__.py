antonagestam/goose

commit 556ccceb1ff56a590596239a91a38f7a1e1d8fdc

fix: Write to files with print
This makes sure there are not missing newlines.

src/goose/__main__.py
@@ -357,7 +357,8 @@ async def git_hook(
     else:
         assert_never(hook)
 
-    hook_path.write_text(template(config_path=config_path))
+    with hooks_path.open("w") as fd:
+        print(template(config_path=config_path), file=fd)
     hook_path.chmod(0o755)
 
     console.print(

src/goose/backend/node.py
@@ -75,7 +75,8 @@ def _write_package_json(
             for dependency in config.dependencies
         }
     )
-    package_json_path.write_text(package_json.model_dump_json(by_alias=True))
+    with package_json_path.open("w") as fd:
+        print(package_json.model_dump_json(by_alias=True), file=fd)
     return package_json_path
 
 

src/goose/environment.py
@@ -68,7 +68,8 @@ def read_state(env_dir: Path) -> State:
 
 def write_state(env_dir: Path, state: SyncedState | InitialState) -> None:
     state_file = env_dir / "goose-state.json"
-    state_file.write_text(state.model_dump_json())
+    with state_file.open("w") as fd:
+        print(state.model_dump_json(), file=fd)
 
 
 @final

src/goose/manifest.py
@@ -106,7 +106,8 @@ def write_manifest(
     manifest: LockManifest,
 ) -> None:
     manifest_path = lock_files_path / "manifest.json"
-    manifest_path.write_text(manifest.model_dump_json())
+    with manifest_path.open("w") as fd:
+        print(manifest.model_dump_json(), file=fd)
     print(f"Wrote manifest to {manifest_path}", file=sys.stderr)
 
 

commit 75e0ebf5d8f153321ac40b630e10ef87109a8ca5

fix: Open correct hook path
556ccce introduced a bug and picked the wrong path to write to.

src/goose/__main__.py
@@ -357,7 +357,7 @@ async def git_hook(
     else:
         assert_never(hook)
 
-    with hooks_path.open("w") as fd:
+    with hook_path.open("w") as fd:
         print(template(config_path=config_path), file=fd)
     hook_path.chmod(0o755)
 